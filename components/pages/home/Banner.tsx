262d1e042bf462142da61f6bf8bb487de830806f
ThanosC0de <sahilthummar257@gmail.com>
2025-06-28 17:57:21 +0530

    done

--- components/pages/home/Banner.tsx
+++ components/pages/home/Banner.tsx
@@ -1,19 +1,32 @@
-import Container from '@/components/common/Container'
-import { Carousel, CarouselContent } from '@/components/ui/carousel'
-import React from 'react'
+import Container from "@/components/common/Container";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { getBanner } from "@/sanity/queries/indext";
+import React from "react";
 
-const Banner = () => {
+const Banner = async () => {
+  const banner = await getBanner();
+
   return (
-    <Container className='grid grid-cols-1 lg:grid-cols-4 gap-4 md:gap-5'>
-        <div className='w-full lg:col-span-3'>
-            <Carousel>
-                <CarouselContent>
-                    
-                </CarouselContent>
-            </Carousel>
-        </div>
+    <Container className="grid grid-cols-1 lg:grid-cols-4 gap-4 md:gap-5">
+      <div className="w-full lg:col-span-3">
+        <Carousel>
+          <CarouselContent>
+            {banner?.map((item, index) => (
+              <CarouselItem key={index}>
+                <div>
+                  <p>hello</p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,6 +4,8 @@
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
+
+
 export const { sanityFetch, SanityLive } = defineLive({ 
   client: client,
   fetchOptions:{
