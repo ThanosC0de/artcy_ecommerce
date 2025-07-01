4e353a50dfa94aa0a1ad91c1c9bb7ed707997d74
ThanosC0de <sahilthummar257@gmail.com>
2025-07-01 17:15:44 +0530

    done

--- components/common/GoBack.tsx
+++ components/common/GoBack.tsx
@@ -8,7 +8,7 @@ const GoBack = () => {
     <div className="absolute top-5 left-5 md:top-10 md:left-10">
       <button onClick={()=> router?.back()} className="flex items-center gap-0.5 hover:text-tech_orange hover:cursor-pointer hoverEffext">
         <ChevronLeft size={18}/>
-        <span>GO back</span>
+        <span>Go back</span>
       </button>
     </div>
   );
--- components/common/ProductCard.tsx
+++ components/common/ProductCard.tsx
@@ -1,17 +1,44 @@
-import { Product } from '@/sanity.types'
-import { image } from '@/sanity/image'
-import Link from 'next/link'
-import React from 'react'
+import { Product } from "@/sanity.types";
+import { image } from "@/sanity/image";
+import Link from "next/link";
+import React from "react";
 
-const ProductCard = ({product}:{product:Product}) => {
+const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <div>
-        <div>
-            {product?.images && <Link href={`/product/${product?.slug?.current}`}>
-                <image src={image(product?.images[0]).size(900,700).url()} alt="" /></Link>}
-        </div>
+    <div className="text-sm rounded-md group bg-tech_white ">
+      <div className="relative overflow-hidden">
+        {product?.images && (
+          <Link href={`/product/${product?.slug?.current}`}>
+            <img
+              src={image(product?.images[0]).size(900, 900).url()}
+              alt="productImage"
+              loading="lazy"
+              className={`w-full h-70 object-contain rounde overflow-hidden 
+                ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-60"}hoverEffect`}
+            />
+          </Link>
+        )}
+        {(product?.discount as number) > 0 && (
+          <p
+            className="absolute top-2 left z-10 text-xs border border-l-0 
+            bg-tech_purple text-tech_white font-semibold
+             border-texh_dark/50 rounded-r-full px-2"
+          >
+            Save:₹
+            {Math.round(
+              (product?.price as number) * ((product?.discount as number) / 100)
+            )}{" "}
+            {`-(${product?.discount})`}
+          </p>
+        )}
+      </div>
+      <div>
+        {product?.categories && (
+          <p>{product?.categories.map((cat) => cat).join(",")}</p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
